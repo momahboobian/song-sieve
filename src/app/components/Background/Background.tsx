23795d5874199826815a9a653c8cad437d6e428f
Mo Mahboobian <moreelltd@gmail.com>
2025-03-06 12:28:33 +0000

    feat: Add dynamic background

---
+++ src/app/components/Background/Background.tsx
@@ -0,0 +1,23 @@
+"use client";
+
+import "./Background.css";
+
+const DynamicBackground = () => {
+  return (
+    <div className="background">
+      <span></span>
+      <span></span>
+      <span></span>
+      <span></span>
+      <span></span>
+      <span></span>
+      <span></span>
+      <span></span>
+      <span></span>
+      <span></span>
+      <span></span>
+    </div>
+  );
+};
+
+export default DynamicBackground;
